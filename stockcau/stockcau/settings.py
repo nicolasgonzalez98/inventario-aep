35397697e520400c079b2af5283114ed290c81ed
GONZALEZ <nicolasgonzalez470@gmail.com>
2023-11-01 16:00:37 -0300

    cambios

--- stockcau/stockcau/settings.py
+++ stockcau/stockcau/settings.py
@@ -139,7 +139,7 @@ USE_L10N = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 # Default primary key field type
