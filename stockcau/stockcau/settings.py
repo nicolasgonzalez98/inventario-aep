ed625f477a8a402006bd8ada99102dab57f64faf
Nicolas Nahuel Gonzalez <nicolasgonzalez470@gmail.com>
2023-11-22 01:15:50 -0300

    cambios

--- stockcau/stockcau/settings.py
+++ stockcau/stockcau/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_b@e^!97x-w20!#1diffuo^69fqz0zhp)f39_l86c6hhvq3s_d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "*"
@@ -138,9 +138,14 @@ USE_L10N = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_URL = '/static_root/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+    # Add more directories if needed
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
